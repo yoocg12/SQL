ca35318b0a17784058bbcc386fd4ad4f912e5a51
yoocg12 <70730681+yoocg12@users.noreply.github.com>
2020-11-06 08:17:43 +0900

    Create alter table.sql

---
+++ Oracle/alter table.sql
@@ -0,0 +1,16 @@
+--tmp02 table 생성
+create table tmp02
+(
+    a varchar2(100)
+    b varchar2(100)
+)
+
+--tmp02 table 에 varchar2 Type의 c column 추가. 최대 100Byte까지 입력이 가능.
+--한글은 한 글자에 2Byte, 영어나 숫자는 1Byte
+
+Alter table tmp02 add c varchar2(100);
+
+
+--c column 정상 추가 확인
+select *
+  from tmp02
